eb11cd31094b028786b8c89e05f3f16df6a6c8b8
jeong-hae-rim <usemono33@gmail.com>
2024-01-03 21:26:45 +0900

    feat: 카테고리 전체 조회 api 구현

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -2,7 +2,23 @@ const conn = require("../db/mariadb");
 const { StatusCodes } = require("http-status-codes");
 
 const allReadCategory = (req, res) => {
-    res.json({ message: "카테고리 전체 조회" })
+    let allReadSql = "SELECT * FROM categories";
+
+    conn.query(allReadSql, (err, results) => {
+        if (err) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                message: err
+            })
+        }
+
+        const categories = results[0];
+
+        if (categories) {
+            return res.status(StatusCodes.OK).json(results);
+        } else {
+            return res.status(StatusCodes.NOT_FOUND).end();
+        }
+    })
 }
 
 module.exports = allReadCategory;
