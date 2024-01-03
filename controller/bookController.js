8614cfaf08bda022a885bf066c57de4b22f6694b
jeong-hae-rim <usemono33@gmail.com>
2024-01-03 20:43:46 +0900

    feat: 전체 도서 조회 api, 상세 도서 조회 api 구현

--- controller/bookController.js
+++ controller/bookController.js
@@ -2,11 +2,47 @@ const conn = require("../db/mariadb");
 const { StatusCodes } = require("http-status-codes");
 
 const allBooks = (req, res) => {
-    res.json({ message: "전체 도서 조회" })
+    let sql = "SELECT * FROM books";
+
+    conn.query(sql, (err, results) => {
+        if (err) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                message: err
+            })
+        }
+
+        const books = results[0];
+
+        if (books) {
+            return res.status(StatusCodes.OK).json(results);
+        } else {
+            return res.status(StatusCodes.NOT_FOUND).end();
+        }
+    })
 }
 
 const detailBook = (req, res) => {
-    res.json({ message: "개별 도서 조회" })
+    const { id } = req.params;
+
+    let sql = "SELECT * FROM books WHERE id = ?";
+
+    conn.query(sql, parseInt(id), (err, results) => {
+        if (err) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                message: err
+            })
+        }
+
+        const book = results[0];
+
+        if (book) {
+            return res.status(StatusCodes.OK).json(results);
+        } else {
+            return res.status(StatusCodes.NOT_FOUND).json({
+                massage: "존재하지 않는 도서입니다."
+            });
+        }
+    })
 }
 
 const categoryOfBooks = (req, res) => {
--- routes/books.js
+++ routes/books.js
@@ -6,13 +6,12 @@ const {
 } = require("../controller/BookController");
 const router = express.Router();
 
-router.use(express.json);
+router.use(express.json());
 
 router.get("/", allBooks);
 
 router.get("/:id", detailBook);
 
 router.get("/:category_id", categoryOfBooks);
-
 
 module.exports = router;
