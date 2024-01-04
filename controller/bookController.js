1f81b18d2cc7d943f8c2600bbab089fe97fb3660
jeong-hae-rim <usemono33@gmail.com>
2024-01-04 19:27:48 +0900

    feat: 카테고리 별 & 신간 도서 목록 조회

--- controller/bookController.js
+++ controller/bookController.js
@@ -2,46 +2,37 @@ const conn = require("../db/mariadb");
 const { StatusCodes } = require("http-status-codes");
 
 const allReadBooks = (req, res) => {
-    const { category_id } = req.query;
-    let categoryReadSql = "SELECT * FROM books WHERE category_id = ?";
-    let allReadSql = "SELECT * FROM books";
-
-    if (category_id) {
-
-        conn.query(categoryReadSql, parseInt(category_id), (err, results) => {
-            if (err) {
-                return res.status(StatusCodes.BAD_REQUEST).json({
-                    message: err
-                })
-            }
-
-            const book = results[0];
-
-            if (book) {
-                return res.status(StatusCodes.OK).json(results);
-            } else {
-                return res.status(StatusCodes.NOT_FOUND).json({
-                    massage: "존재하지 않는 도서입니다."
-                });
-            }
-        })
-    } else {
-        conn.query(allReadSql, (err, results) => {
-            if (err) {
-                return res.status(StatusCodes.BAD_REQUEST).json({
-                    message: err
-                })
-            }
-
-            const books = results[0];
-
-            if (books) {
-                return res.status(StatusCodes.OK).json(results);
-            } else {
-                return res.status(StatusCodes.NOT_FOUND).end();
-            }
-        })
+    const { category_id, recent } = req.query;
+    let sql = "SELECT * FROM books";
+    let values = [];
+    if (category_id && recent) {
+        values = [parseInt(category_id), recent];
+        sql += " WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
+    } else if (category_id) {
+        values = [parseInt(category_id)];
+        sql += " WHERE category_id = ?";
+    } else if (recent) {
+        values = [recent];
+        sql += " WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
     }
+
+    conn.query(sql, values, (err, results) => {
+        if (err) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                message: err
+            })
+        }
+
+        const book = results[0];
+
+        if (book) {
+            return res.status(StatusCodes.OK).json(results);
+        } else {
+            return res.status(StatusCodes.NOT_FOUND).json({
+                massage: "존재하지 않는 도서입니다."
+            });
+        }
+    })
 
 }
 
