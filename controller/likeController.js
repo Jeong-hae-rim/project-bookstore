41d31d4ce3a43498b96cead920f59ef2f74e24d7
jeong-hae-rim <usemono33@gmail.com>
2024-01-05 17:51:50 +0900

    feat: 좋아요 삭제 API 구현

--- controller/likeController.js
+++ controller/likeController.js
@@ -23,7 +23,24 @@ const addLike = (req, res) => {
 };
 
 const removeLike = (req, res) => {
-    res.json({ message: "좋아요 취소" })
+    const { id } = req.params;
+    const { user_id } = req.body;
+
+    let sql = "DELETE FROM likes WHERE user_id = ? AND liked_book_id = ?";
+
+    conn.query(sql, [user_id, parseInt(id)], (err, results) => {
+        if (err) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                message: err
+            })
+        }
+
+        if (results.affectedRows === 0) {
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        } else {
+            return res.status(StatusCodes.OK).json(results);
+        }
+    })
 };
 
 module.exports = { addLike, removeLike }
