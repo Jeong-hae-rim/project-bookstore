8e1f48fb24a002daf38a7999e06097e6154076ec
jeong-hae-rim <usemono33@gmail.com>
2024-01-05 17:48:16 +0900

    feat: 좋아요 추가 API 구현

--- controller/likeController.js
+++ controller/likeController.js
@@ -2,7 +2,24 @@ const conn = require("../db/mariadb");
 const { StatusCodes } = require("http-status-codes");
 
 const addLike = (req, res) => {
-    res.json({ message: "좋아요 추가" })
+    const { id } = req.params;
+    const { user_id } = req.body;
+
+    let sql = "INSERT INTO likes (user_id, liked_book_id) VALUES (?, ?)";
+
+    conn.query(sql, [user_id, parseInt(id)], (err, results) => {
+        if (err) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                message: err
+            })
+        }
+
+        if (results.affectedRows === 0) {
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        } else {
+            return res.status(StatusCodes.OK).json(results);
+        }
+    })
 };
 
 const removeLike = (req, res) => {
