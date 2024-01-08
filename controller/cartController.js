5e203ad577765ee00ecd03bce6f5b0f4b28314a3
jeong-hae-rim <usemono33@gmail.com>
2024-01-08 21:14:14 +0900

    feat: 장바구니 조회 api 구현

--- controller/cartController.js
+++ controller/cartController.js
@@ -2,7 +2,26 @@ const conn = require("../db/mariadb");
 const { StatusCodes } = require("http-status-codes");
 
 const allReadCartItems = (req, res) => {
-    res.json({ message: "장바구니 조회" })
+    const { user_id } = req.body;
+
+    let allReadSql = `SELECT CART_ITEMS_TB.id, book_id, title, summary, amount, price 
+                    FROM CART_ITEMS_TB LEFT JOIN BOOKS_TB 
+                    ON CART_ITEMS_TB.book_id = BOOKS_TB.id
+                    WHERE user_id = ?`;
+
+    conn.query(allReadSql, parseInt(user_id), (err, results) => {
+        if (err) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                message: err
+            })
+        }
+
+        if (results[0]) {
+            return res.status(StatusCodes.OK).json(results);
+        } else {
+            return res.status(StatusCodes.NOT_FOUND).end();
+        }
+    })
 }
 
 const addToCarts = (req, res) => {
