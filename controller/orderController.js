7229ab6a1285952c0dafe231b6d57b80971d3d9b
jeong-hae-rim <usemono33@gmail.com>
2024-01-09 19:27:11 +0900

    feat: 주문하기 api 구현

--- controller/orderController.js
+++ controller/orderController.js
@@ -7,7 +7,62 @@ const readAllOrder = (req, res) => {
 }
 
 const addToOrder = (req, res) => {
-    res.json({ message: "결제하기" })
+    const { items, delivery, total_amount, total_price, user_id, first_book_title } = req.body;
+
+    // DELIVERY_TB INSERT (DELIVERY_TB id 있어야 함)
+    let deliverySql = "INSERT INTO DELIVERY_TB (address, receiver, contact) VALUES (?, ?, ?)";
+    let deliveryValues = [delivery.address, delivery.receiver, delivery.contact];
+
+    // ORDERS_TB INSERT (ORDERS_TB id 있어야 함)
+    let ordersSql = `INSERT INTO ORDERS_TB (book_title, total_amount, total_price, user_id, delivery_id) VALUES (?, ?, ?, ?, ?)`;
+    let ordersValues = [first_book_title, total_amount, total_price, user_id];
+
+    // ORDERED_BOOKS_TB INSERT (가장 마지막에 되어야 함)
+    let orderedBooksSql = `INSERT INTO ORDERED_BOOKS_TB (order_id, book_id, amount) VALUES ?`;
+    let orderedBooksValues = [];
+
+    // 쿼리 실행
+    conn.query(deliverySql, deliveryValues, (err, deliveryResults) => {
+        if (err) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                message: err
+            });
+        }
+
+        // 결과에서 delivery_id를 가져올 수 있으면 가져오기
+        const delivery_id = deliveryResults.insertId;
+
+        // ORDERS_TB 쿼리 계속 진행
+        conn.query(ordersSql, [...ordersValues, delivery_id], (err, ordersResults) => {
+            if (err) {
+                return res.status(StatusCodes.BAD_REQUEST).json({
+                    message: err
+                });
+            }
+
+            // 결과에서 order_id를 가져올 수 있으면 가져오기
+            const order_id = ordersResults.insertId;
+
+            // ORDERED_BOOKS_TB 쿼리 계속 진행
+            items.forEach((item) => {
+                orderedBooksValues.push([order_id, item.book_id, item.amount]);
+            });
+
+            conn.query(orderedBooksSql, [orderedBooksValues], (err, orderedBooksResults) => {
+                if (err) {
+                    return res.status(StatusCodes.BAD_REQUEST).json({
+                        message: err
+                    });
+                }
+
+                if (orderedBooksResults.affectedRows === 0) {
+                    return res.status(StatusCodes.BAD_REQUEST).end();
+                } else {
+                    return res.status(StatusCodes.OK).json(orderedBooksResults);
+                }
+            });
+        });
+    });
 }
 
 const readDetailOrder = (req, res) => {
