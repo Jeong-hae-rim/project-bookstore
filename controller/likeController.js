e159ce74a5fb72953c1df8d44b7886bb22626a19
jeong-hae-rim <usemono33@gmail.com>
2024-01-15 21:45:45 +0900

    feat: like api 핸들러 async-await 추가 및 로직 수정

--- controller/likeController.js
+++ controller/likeController.js
@@ -1,46 +1,34 @@
 const conn = require("../db/mariadb");
 const { StatusCodes } = require("http-status-codes");
 
-const addLike = (req, res) => {
+const addLike = async (req, res) => {
     const { id } = req.params;
     const { user_id } = req.body;
 
     let sql = "INSERT INTO LIKES_TB (user_id, liked_book_id) VALUES (?, ?)";
 
-    conn.query(sql, [user_id, parseInt(id)], (err, results) => {
-        if (err) {
-            return res.status(StatusCodes.BAD_REQUEST).json({
-                message: err
-            })
-        }
+    let [results, fields] = await conn.query(sql, [user_id, parseInt(id)]);
 
-        if (results.affectedRows === 0) {
-            return res.status(StatusCodes.BAD_REQUEST).end();
-        } else {
-            return res.status(StatusCodes.OK).json(results);
-        }
-    })
+    if (results.affectedRows === 0) {
+        return res.status(StatusCodes.BAD_REQUEST).end();
+    } else {
+        return res.status(StatusCodes.OK).json(results);
+    }
 };
 
-const removeLike = (req, res) => {
+const removeLike = async (req, res) => {
     const { id } = req.params;
     const { user_id } = req.body;
 
     let sql = "DELETE FROM LIKES_TB WHERE user_id = ? AND liked_book_id = ?";
 
-    conn.query(sql, [user_id, parseInt(id)], (err, results) => {
-        if (err) {
-            return res.status(StatusCodes.BAD_REQUEST).json({
-                message: err
-            })
-        }
+    let [results, fields] = await conn.query(sql, [user_id, parseInt(id)]);
 
-        if (results.affectedRows === 0) {
-            return res.status(StatusCodes.BAD_REQUEST).end();
-        } else {
-            return res.status(StatusCodes.OK).json(results);
-        }
-    })
+    if (results.affectedRows === 0) {
+        return res.status(StatusCodes.BAD_REQUEST).end();
+    } else {
+        return res.status(StatusCodes.OK).json(results);
+    }
 };
 
 module.exports = { addLike, removeLike }
