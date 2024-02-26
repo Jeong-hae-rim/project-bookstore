148315dfdb35cb60ca171e129782f9b40ec2efd5
jeong-hae-rim <usemono33@gmail.com>
2024-02-26 22:55:29 +0900

    feat: category mvc 패턴 적용 및 cors 설정

--- app.ts
+++ app.ts
@@ -1,14 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import booksRouter from "@router/book.router";
 import cartsRouter from "@router/carts";
-import categoryRouter from "@router/category";
+import categoryRouter from "@router/category.router";
 import likesRouter from "@router/likes";
 import ordersRouter from "@router/orders";
 import usersRouter from "@router/users";
 
 dotenv.config();
+
+const corsOptions = {
+    origin: "http://localhost:3000",
+    credentials: true,
+};
 
 let app = express();
 app.use(express.urlencoded({ extended: false }));
@@ -19,9 +25,9 @@ app.listen(process.env.PORT, () => {
     console.log("**----------------------------------------**");
 });
 
-app.use("/books", booksRouter);
-app.use("/carts", cartsRouter);
-app.use("/category", categoryRouter);
-app.use("/likes", likesRouter);
-app.use("/orders", ordersRouter);
-app.use("/users", usersRouter);
+app.use("/books", cors(corsOptions), booksRouter);
+app.use("/carts", cors(corsOptions), cartsRouter);
+app.use("/category", cors(corsOptions), categoryRouter);
+app.use("/likes", cors(corsOptions), likesRouter);
+app.use("/orders", cors(corsOptions), ordersRouter);
+app.use("/users", cors(corsOptions), usersRouter);
